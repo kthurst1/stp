09e18a40403f8e978c60ee792bddd0f5110b8139
c-l-scholl <cscholl@macalester.edu>
2022-10-11 15:54:55 -0500

    can now console.log a song from firestore

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getFirestore, collection, getDocs, doc, getDoc } from "firebase/firestore";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-app.js";
+//import { getAnalytics } from "firebase/analytics";
+import { getFirestore, collection, getDocs, doc, getDoc } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-firestore.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyC17ZK7zVyj0BrG7f3jk7fTPrGxMKwgraw",
   authDomain: "stpcomp225project.firebaseapp.com",
@@ -14,37 +15,31 @@ const firebaseConfig = {
 
 // init app
 const app = initializeApp(firebaseConfig);
-const firebase = require("firebase");
+//const firebase = require("firebase");
 // Required for side-effects
-require("firebase/firestore");
+//require("firebase/firestore");
 
 // init services
-const analytics = getAnalytics(app);
+//const analytics = getAnalytics(app);
 const db = getFirestore();
 
 // collection ref
 const colRef = collection(db, 'spotifydata');
 
 //get collection data
-getDocs(colRef).then((snapshot) => {
-  console.log(snapshot);
-  let songData = [];
-  snapshot.docs.forEach((doc) => {
-    songData.push({ ...doc.data(), id: doc.id });
-  })
-})
-.catch(err => {
-  console.log(err.message);
-});
 
 
+// const data = await getDocs(collection(db, "spotifydata"));
+// let songData = [];
+// data.forEach((doc) => {
+//   songData.push(doc.data());
+// })
 
-// const docRef = doc(db, "spotifydata");
-// const docSnap = await getDoc(docRef);
+const spotifydata = doc(db, "spotifydata/00Th9LffyzEEqICCin7G");
+const doc1 = await getDoc(spotifydata);
+if (!doc1.exists) {
+  console.log('No such document!');
+} else {
+  console.log('Document data:', doc1.data());
+}
 
-// if (docSnap.exists()) {
-//   console.log("Document data:", docSnap.data());
-// } else {
-//   // doc.data() will be undefined in this case
-//   console.log("No such document!");
-// }
--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,6 @@ const path = require('path');
 
 module.exports = {
     entry: './src/index.js',
-    devtool: false,
-    mode: 'development',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
